1579e589fdde9061b833679eb8604b1d3196e63f
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2022-11-12 09:37:08 +0900

    Refactor: simplify sns topic policy

--- terraform/module/notification/sns.tf
+++ terraform/module/notification/sns.tf
@@ -2,40 +2,20 @@ resource "aws_sns_topic" "notification" {
   name = "ci-pull-request-notification"
 }
 
-# TODO あとでなおす
-# resource "aws_sns_topic_policy" "notification" {
-#   arn = aws_sns_topic.notification.arn
-#   policy = jsonencode({
-#     "Effect" : "Allow",
-#     "Principal" : {
-#       "Service" : [
-#         "codestar-notifications.amazonaws.com"
-#       ]
-#     },
-#     "Action" : "SNS:Publish",
-#     "Resource" : aws_sns_topic.notification.arn,
-#     "Condition" : {
-#       "StringEquals" : {
-#         "aws:SourceAccount" : data.aws_caller_identity.current.account_id
-#       }
-#     }
-#   })
-# }
-
-data "aws_iam_policy_document" "notification_access" {
-  statement {
-    actions = ["sns:Publish"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["codestar-notifications.amazonaws.com"]
-    }
-
-    resources = [aws_sns_topic.notification.arn]
-  }
-}
-
-resource "aws_sns_topic_policy" "default" {
-  arn    = aws_sns_topic.notification.arn
-  policy = data.aws_iam_policy_document.notification_access.json
+resource "aws_sns_topic_policy" "notification" {
+  arn = aws_sns_topic.notification.arn
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "codestar-notifications.amazonaws.com"
+        },
+        "Action" : "sns:Publish",
+        "Resource" : aws_sns_topic.notification.arn
+      }
+    ]
+  })
 }
